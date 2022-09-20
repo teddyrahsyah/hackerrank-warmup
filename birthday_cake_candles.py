c80320909b98011cf0c554f1d3537b1f22032463
teddyrahsyah212 <teddyrahsyah@gmail.com>
2022-09-20 22:51:59 +0700

    birthday cake candles solved

---
+++ birthday_cake_candles.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    maxHeight = max(candles)
+    print(candles.count(maxHeight))
+
+if __name__ == '__main__':
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
